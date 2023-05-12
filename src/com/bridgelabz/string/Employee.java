84f8e9fdf1f9c8780a32753f56ee9c6e8dbea2fe
julinbestina <julin.bestina@yahoo.com>
2023-05-12 19:56:37 +0530

    Added:Code to check Employee attendance

--- src/com/bridgelabz/string/Employee.java
+++ src/com/bridgelabz/string/Employee.java
@@ -1,8 +1,17 @@
 package com.bridgelabz.string;
 
+import java.util.Random;
+
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program");
+        int empCheck = (int)(Math.random()*10)%2;
+         if(empCheck == 1) {
+             System.out.println("Employee is present");
+         }
+         else {
+             System.out.println("Employee is absent");
+         }
     }
 
 }
